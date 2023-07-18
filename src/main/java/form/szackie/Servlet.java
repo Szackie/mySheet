abc8877b48afbf215158b6e580ee68b951d6879c
Szymon Zackiewicz <zacszym@gmail.com>
2023-07-18 20:29:41 +0200

    user ID map

--- src/main/java/form/szackie/Servlet.java
+++ src/main/java/form/szackie/Servlet.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -12,75 +13,82 @@ import java.util.stream.Collectors;
 @RequestMapping("/api")
 public class Servlet {
 
-    List<Form> availableForms = new ArrayList<>();
-    List<Form> formsToCreate = new ArrayList<>();
+    private Map<String, List<Form>> availableFormsMap = new HashMap<>();
+    private Map<String, List<Form>> formsToCreateMap = new HashMap<>();
+    private Map<String, Service> serviceMap = new HashMap<>();
 
-    Service service;
-
-//ŚCINKI
+    // ŚCINKI
 
     @PostMapping("/scinka")
-    ResponseEntity<List<Form>> addNewWaste(@RequestBody Map<String, String> wasteText) {
+    ResponseEntity<List<Form>> addNewWaste(@RequestHeader("userId") String userId, @RequestBody Map<String, String> wasteText) {
+        List<Form> availableForms = availableFormsMap.getOrDefault(userId, new ArrayList<>());
+
         String str = wasteText.get("name");
         List<Form> newWastes = Service.tokenizer(str, 2);
         availableForms.addAll(newWastes);
+
+        availableFormsMap.put(userId, availableForms);
+
         return ResponseEntity.ok(availableForms);
     }
 
     @DeleteMapping("/scinka/{id}")
-    ResponseEntity<List<Form>> removeWaste(@PathVariable int id) {
+    ResponseEntity<List<Form>> removeWaste(@RequestHeader("userId") String userId, @PathVariable int id) {
+        List<Form> availableForms = availableFormsMap.get(userId);
         if (availableForms == null)
             return ResponseEntity.notFound().build();
-        for (int i = 0; i < availableForms.size(); i++) {
-            if (availableForms.get(i).getId() == id) {
-                availableForms.remove(i);
-                break;
-            }
-        }
+
+        availableForms = availableForms.stream().filter(form -> form.getId() != id).collect(Collectors.toList());
+        availableFormsMap.put(userId, availableForms);
+
         return ResponseEntity.ok(availableForms);
     }
 
-//FORMATKI
+    // FORMATKI
 
     @PostMapping("/formatka")
-    ResponseEntity<List<Form>> addNewForm(@RequestBody Map<String, String> formText) {
+    ResponseEntity<List<Form>> addNewForm(@RequestHeader("userId") String userId, @RequestBody Map<String, String> formText) {
+        List<Form> formsToCreate = formsToCreateMap.getOrDefault(userId, new ArrayList<>());
+
         String str = formText.get("name");
         List<Form> newForms = Service.tokenizer(str, 1);
         formsToCreate.addAll(newForms);
+
+        formsToCreateMap.put(userId, formsToCreate);
+
         return ResponseEntity.ok(formsToCreate);
     }
 
     @DeleteMapping("/formatka/{id}")
-    ResponseEntity<List<Form>> removeForm(@PathVariable int id) {
+    ResponseEntity<List<Form>> removeForm(@RequestHeader("userId") String userId, @PathVariable int id) {
+        List<Form> formsToCreate = formsToCreateMap.get(userId);
         if (formsToCreate == null)
             return ResponseEntity.notFound().build();
-        for (int i = 0; i < formsToCreate.size(); i++) {
-            if (formsToCreate.get(i).getId() == id) {
-                formsToCreate.remove(i);
-                break;
-            }
-        }
+
+        formsToCreate = formsToCreate.stream().filter(form -> form.getId() != id).collect(Collectors.toList());
+        formsToCreateMap.put(userId, formsToCreate);
+
         return ResponseEntity.ok(formsToCreate);
     }
 
     @GetMapping("/solve")
+    public ResponseEntity<Service> solution(@RequestHeader("userId") String userId) {
+        List<Form> availableForms = availableFormsMap.getOrDefault(userId, new ArrayList<>());
+        List<Form> formsToCreate = formsToCreateMap.getOrDefault(userId, new ArrayList<>());
 
-    public ResponseEntity<Service> solution(){
-        service=new Service(availableForms,formsToCreate);
+        Service service = new Service(availableForms, formsToCreate);
+        serviceMap.put(userId, service);
+
         var result = service.solve();
         return ResponseEntity.ok(result);
     }
 
     @DeleteMapping("/reset")
-    public ResponseEntity<?> removeAllData(){
-        if(formsToCreate!=null)
-        formsToCreate.clear();
-        if(availableForms!=null)
-        availableForms.clear();
-        if(service!=null)
-        service.reset();
+    public ResponseEntity<?> removeAllData(@RequestHeader("userId") String userId) {
+        availableFormsMap.remove(userId);
+        formsToCreateMap.remove(userId);
+        serviceMap.remove(userId);
 
         return ResponseEntity.ok().build();
     }
-
 }
